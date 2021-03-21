43c13939064b952e210ecbd98d14ec02c72317d4
Dave Rupert <rupato@gmail.com>
2021-03-21 12:08:23 -0500

    Half-fix build server

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite'
+const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    lib: {
-      entry: 'src/my-element.js',
-      formats: ['es']
-    },
+    // lib: {
+    //   entry: 'src/custom-alert.js',
+    //   formats: ['es']
+    // },
     rollupOptions: {
-      external: /^lit-element/
+      external: /^lit-element/,
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        alert: resolve(__dirname, 'alert.html'),
+        aframe: resolve(__dirname, 'aframe.html'),
+        modelviewer: resolve(__dirname, 'model-viewer.html'),
+        twoup: resolve(__dirname, 'two-up.html')
+      },
     }
   }
 })
